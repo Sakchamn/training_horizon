ae3a63794fba99427294bf92cd53899953210ab0
Sakchamn <143090295+Sakchamn@users.noreply.github.com>
2024-08-12 17:30:47 +0530

    Hero

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -4,9 +4,9 @@ import Button from '@/components/Button';
 
 const Header = () => {
   return (
-    <header className="bg-[#56C1FF] w-screen h-screen bg-cover bg-bottom bg-no-repeat text-white py-48 text-center p-10"
+    <header className="bg-[#56C1FF] bg-cover bg-bottom bg-no-repeat text-white py-48 text-center p-10"
     style={{ backgroundImage: "url('/images/HeroBg.png')" }}>
-    <div className=" flex justify-center items-center">
+    <div className="w-screen flex justify-center items-center">
       <div className="w-1/2">
       <header className=" py-20 text-center">
         <h1 className="text-4xl  bold-64 text-shadow-sm mb-4 text-white">Get your skills upgraded with us</h1>
@@ -24,16 +24,3 @@ const Header = () => {
 
 export default Header;
 
-
-
-// <header className="flex justify-between items-center p-5 bg-white shadow-md">
-//       <div className="text-xl font-bold">Your Logo</div>
-//       <nav>
-//         <ul className="flex space-x-5">
-//           <li><a href="#">Home</a></li>
-//           <li><a href="#">Courses</a></li>
-//           <li><a href="#">About</a></li>
-//           <li><a href="#">Contact</a></li>
-//         </ul>
-//       </nav>
-//     </header>
